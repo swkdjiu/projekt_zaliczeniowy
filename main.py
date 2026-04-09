f92de79a831b2c09c56d5935fd6a034fc1c8af55
Yana Fefelova <fefelovaa013@gmail.com>
2026-04-09 17:37:32 +0200

    Dodanie zmian w main.py - input i start gry

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from src.game import zagraj_runde
+
+def main():
+    print("Witaj w grze: Kamień, Nożyce, Papier!")
+
+    imie1 = input("Podaj imię gracza 1: ")
+    imie2 = input("Podaj imię gracza 2: ")
+
+    zagraj_runde(imie1, imie2)
+
 if __name__ == "__main__":
-    print("Witaj w grze Kamień, Nożyce, Papier!")
+    main()
